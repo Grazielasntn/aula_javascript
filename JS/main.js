f1a7d01a049cf9b6690770615cdbebea74acc877
graziela <grazielasntnalves@gmail.com>
2021-10-06 23:40:51 -0300

    colocando projeto no git

---
+++ JS/main.js
@@ -0,0 +1,49 @@
+
+function clicou(){
+    //alert("Obrigada por clicar");
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigada por clicar</b>";
+}
+function redirecionar(){
+//para abrir em outra página
+  //  window.open("https://www.linkedin.com/in/graziela-santana-3a2327190/");
+  //para abrir na mesma página
+    window.location.href="https://www.linkedin.com/in/graziela-santana-3a2327190/";
+}
+function trocar(){
+    alert("trocar texto");
+}
+//function voltar(){
+    //document.getElementById("mousemove").innerHTML = "<b>Passe o mouse aqui</b>";
+}
+}//document.getElementById("mousemove").innerHTML = "<b>Obrigada por passar o mouse</b>";
+}
+//var nome =  "Graziela Santana";
+//var idade = 27;
+//alert(nome + " tem " + idade + " anos ");
+//var lista = ["maçã" , "uva" , "pêra"];
+//lista.push("banana");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+/*var idade = prompt ("Qual a sua idade?");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+/*var d = new Date();
+alert(d.getDate());
+alert(d.getHours());
+*/
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome,novo_nome)
+}
+alert(soma(5, 10));
+alert(setReplace("Vai Japão","Japão", "Brasil"));
+*/
